ab99bfa859b7753d1c627d18681430ada6417476
Seiryu-alt <seiryusain@gmail.com>
2021-05-05 12:19:09 +0000

    [add] コメント機能のビューを作成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,6 +1,6 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:id])
+    book = Book.find(params[:book_id])
     book_comment = current_user.book_comments.new(book_comment_params)
     book_comment.book_id = book.id
     book_comment.save
@@ -9,6 +9,7 @@ class BookCommentsController < ApplicationController
 
   def destroy
     book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id], user_id: current_user.id)
+    
     if book_comment
       book_comment.destroy
     end
