54f7b1df4df89a48f4a3aeacb3ced3ed7af09a40
Seiryu-alt <seiryusain@gmail.com>
2021-05-06 10:24:53 +0000

    [add] searchアクションの処理を追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,26 @@
 class SearchController < ApplicationController
   def search
-    puts "----------------------params------------------------"
-    p params
-    puts "----------------------------------------------------"
+    keyword = params[:keyword]
 
+    if params[:search_target] == "Book"
+      target_model = Book
+      target_column = "title"
+    else
+      target_model = User
+      target_column = "name"
+    end
+
+    case params[:match_type]
+    when "1"
+      search_keyword = params[:keyword]
+    when "2"
+      search_keyword = params[:keyword] + "%"
+    when "3"
+      search_keyword = "%" + params[:keyword]
+    when "4"
+      search_keyword = "%" + params[:keyword] + "%"
+    end
+
+    @results = target_model.where("#{target_column} like ?", search_keyword)
   end
 end
