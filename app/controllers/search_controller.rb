e8b3470bbd4098cfde84820200f508e6ff5bacfb
Seiryu-alt <seiryusain@gmail.com>
2021-05-06 10:40:59 +0000

    [add] 検索結果のページを作成

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,15 +12,19 @@ class SearchController < ApplicationController
 
     case params[:match_type]
     when "1"
-      search_keyword = params[:keyword]
+      @search_keyword = params[:keyword]
     when "2"
-      search_keyword = params[:keyword] + "%"
+      @search_keyword = params[:keyword] + "%"
     when "3"
-      search_keyword = "%" + params[:keyword]
-    when "4"
-      search_keyword = "%" + params[:keyword] + "%"
+      @search_keyword = "%" + params[:keyword]
+    else "4"
+      @search_keyword = "%" + params[:keyword] + "%"
     end
 
-    @results = target_model.where("#{target_column} like ?", search_keyword)
+    @results = target_model.where("#{target_column} like ?", @search_keyword)
+    puts "----------------------params------------------------"
+    p @results.model == User
+    puts "----------------------------------------------------"
+
   end
 end
