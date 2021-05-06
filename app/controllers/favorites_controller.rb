58db9eedffc1229ac2ec453b7524a8a9650d0249
Seiryu-alt <seiryusain@gmail.com>
2021-05-06 12:10:43 +0000

    [add] いいね機能を非同期処理に変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,17 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    unless book.favorited_by?(current_user)
-      favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    unless @book.favorited_by?(current_user)
+      favorite = current_user.favorites.new(book_id: @book.id)
       favorite.save
     end
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    if book.favorited_by?(current_user)
-      favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    if @book.favorited_by?(current_user)
+      favorite = current_user.favorites.find_by(book_id: @book.id)
       favorite.destroy
     end
-    redirect_back(fallback_location: root_path)
   end
 end
